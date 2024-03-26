3713b6dbc09a6a362a7b90229d55a543ed4ae943
kasim <kasimsarman@gmail.com>
2024-03-26 17:41:53 +0600

    +js  slider 2 navigatin

--- js/script.js
+++ js/script.js
@@ -288,6 +288,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //slider
 
     const slides = document.querySelectorAll('.offer__slide'),
+          slider = document.querySelector('.offer__slider'),
           prev = document.querySelector('.offer__slider-prev'),
           next = document.querySelector('.offer__slider-next'),
           current = document.querySelector('#current')
@@ -318,6 +319,70 @@ window.addEventListener('DOMContentLoaded', () => {
         slyde.style.width = width
     })
 
+    slider.style.position = 'relative'
+
+    const indicators = document.createElement('ol'),
+          dots = []
+        ;
+
+    indicators.classList.add('carousel-indicators')
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `
+    slider.append(indicators)
+
+    for (let i = 1; i <= slides.length; i++) {
+        const dot = document.createElement('li')
+        dot.setAttribute('data-slide-to', i)
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `
+        if (i == 1) {
+            dot.style.opacity = 1
+        }
+
+        indicators.append(dot)
+        dots.push(dot)
+    }
+
+    function dotsOpacity(){
+        dots.forEach(dot => dot.style.opacity = '.5')
+        dots[slideIndex - 1].style.opacity = '1'
+    }
+
+    function currentNum() {
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex
+        }
+    }
+
+    function transform() {
+        slidesField.style.transform = `translateX(-${offset}px)`
+    }
+
     next.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
             offset = 0
@@ -325,19 +390,15 @@ window.addEventListener('DOMContentLoaded', () => {
             offset += +width.slice(0, width.length - 2)
         }
 
-        slidesField.style.transform = `translateX(-${offset}px)`
-
         if (slideIndex == slides.length) {
             slideIndex = 1
         } else {
             slideIndex ++
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`
-        } else {
-            current.textContent = slideIndex
-        }
+        transform()
+        currentNum()
+        dotsOpacity()
     })
 
     prev.addEventListener('click', () => {
@@ -347,18 +408,28 @@ window.addEventListener('DOMContentLoaded', () => {
             offset -= +width.slice(0, width.length - 2)
         }
 
-        slidesField.style.transform = `translateX(-${offset}px)`
-
         if (slideIndex == 1) {
             slideIndex = slides.length
         } else {
             slideIndex --
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`
-        } else {
-            current.textContent = slideIndex
-        }
+        transform()
+        currentNum()
+        dotsOpacity()
+    })
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to')
+
+            slideIndex = slideTo
+
+            offset = +width.slice(0, width.length - 2) * (slideIndex - 1)
+
+            transform()
+            currentNum()
+            dotsOpacity()
+        })
     })
 })
